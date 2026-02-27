2580326a68c7a433dbb183cb941144921806062f
shadow <shadowknight09812@gmail.com>
2026-02-27 16:47:24 +0530

    Records

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import Hero from '../components/Hero1'
 import Features from '../components/home/feature'
-// import HowItWorks from '../components/home/HowItWorks'
-// import HowItWorks from "../components/home/HowItWorks";
 import HowItWorks from '../components/home/HowItWorks'
 import Impact from '../components/home/Impact'
 
 const Home = () => {
   return (
     <>
-    <Hero/>
-    <Features/>
-    <HowItWorks/>
-    <Impact/>
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Impact />
     </>
-
   )
 }
 
